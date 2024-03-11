ed2bde9575626ec1343da76f4cccb8552ec5d361
Shrey Kariria <shrey.kariria1910@gmail.com>
2024-03-11 18:39:40 +0530

    adding the python file

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello shrey")
+print("Welcome to my calculator")
+a =int(input("Enter the first number : "))
+b =int(input("Enter the second number : "))
+status = input("Enter the operatin you want to perform (+,-,*,/) : ")
+def operation(status):
+    match status:
+        case "+":
+            return a+b
+        case "-":
+            return a-b
+        case "*":
+            return a*b
+        case "/":
+            if b != 0:
+                return a / b
+            else:
+                 return "Error: Division by zero"
+        case _:
+            return "Invalid operation"
+
+
+result = operation(status)
+print("Result:", result)
